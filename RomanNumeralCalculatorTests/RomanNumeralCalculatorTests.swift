65b50c9438bc1040db07b21b198be6e3a03dc257
theGitUser <sgopu@unm.edu>
2017-07-27 15:54:09 -0600

    add toRoman unit tests

--- RomanNumeralCalculatorTests/RomanNumeralCalculatorTests.swift
+++ RomanNumeralCalculatorTests/RomanNumeralCalculatorTests.swift
@@ -2,11 +2,12 @@
 //  RomanNumeralCalculatorTests.swift
 //  RomanNumeralCalculatorTests
 //
-//  Created by Jarrod Parkes on 11/29/16.
-//  Copyright © 2016 buddybuild. All rights reserved.
+//  Created by Satyanarayana Gopu on 7/27/17.
+//  Copyright © 2017 buddybuild. All rights reserved.
 //
 
 import XCTest
+@testable import RomanNumeralCalculator
 
 class RomanNumeralCalculatorTests: XCTestCase {
     
@@ -20,9 +21,13 @@ class RomanNumeralCalculatorTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func testToRoman() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertEqual(toRoman(1), "I")
+        XCTAssertEqual(toRoman(5), "V")
+        XCTAssertEqual(toRoman(10), "X")
+        
     }
     
     func testPerformanceExample() {
